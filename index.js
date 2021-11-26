01cec4721a1adf954a9eca0011b3ba68a674486f
jonathanLum <loomsden345@gmail.com>
2021-11-26 00:56:58 -0500

    page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
