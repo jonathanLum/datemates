9c8cf2469af7a6a01d976df3a54d9f2c2808cdf4
jonathanLum <loomsden345@gmail.com>
2021-11-26 01:18:27 -0500

    fgf

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
 const port = process.env.PORT || 3000
+app.use(express.static(__dirname + '/'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', port);
 
 
-app.get('/', function(req, res) {
-  res.send('Hello World!')
+
+
+
+app.get('/',function(req,res){
+  res.render('home');
 });
 
-app.listen(port, function() {
-  console.log(`Example app listening on port ${port}!`)
+
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://flip3.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
