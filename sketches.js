f18b48254270daea38223874fba366acb1e28635
jonathanLum <loomsden345@gmail.com>
2021-11-28 23:28:36 -0500

    dffdf

--- sketches.js
+++ sketches.js
@@ -1,7 +1,9 @@
-function sketch1(){
-    function setup() {
-        let c = createCanvas(100, 100);
-        background(255, 0, 0);
-        saveCanvas('myCanvas', 'jpg');
+module.exports = {
+    sketch1: function (){
+        function setup() {
+            let c = createCanvas(100, 100);
+            background(255, 0, 0);
+            saveCanvas('myCanvas', 'jpg');
+        }
     }
 }
